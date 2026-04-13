472549c43cf7c5dafb6ad93edde363fad6bf3358
Mehdi <mehdijmlkh1382@gmail.com>
2026-04-13 19:11:17 +0330

    refactor: create static methods for creating response object

--- src/main/java/ir/ac/ut/ece/ie/users/Response.java
+++ src/main/java/ir/ac/ut/ece/ie/users/Response.java
@@ -1,11 +1,21 @@
 package ir.ac.ut.ece.ie.users;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @AllArgsConstructor
 @Data
+@Builder
 public class Response {
     private boolean success;
     private  String message;
+
+    public static Response ok(String message) {
+        return new Response(true, message);
+    }
+
+    public static Response failed(String message) {
+        return new Response(false, message);
+    }
 }
--- src/main/java/ir/ac/ut/ece/ie/users/UserService.java
+++ src/main/java/ir/ac/ut/ece/ie/users/UserService.java
@@ -1,9 +1,7 @@
 package ir.ac.ut.ece.ie.users;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.mapstruct.factory.Mappers;
 
 @AllArgsConstructor
@@ -14,33 +12,33 @@ public class UserService {
     public Response addUser(@Valid AddUserRequest request) {
         var user = userRepository.findByUsername(request.getUsername()).orElse(null);
         if (user != null) {
-            return new Response(false, "A user with this username already exists.");
+            return Response.failed("A user with this username already exists.");
         }
 
         user = userRepository.findByEmail(request.getEmail()).orElse(null);
         if (user != null) {
-            return new Response(false, "A user with this email already exists.");
+            return Response.failed("A user with this email already exists.");
         }
 
         if (!request.getUsername().matches("^[A-Za-z0-9_-]+$")) {
-            return new Response(false,  "Username is invalid. Only English letters, numbers, -, and _ are allowed.");
+            return Response.failed("Username is invalid. Only English letters, numbers, -, and _ are allowed.");
         }
 
         if (!request.getEmail().matches("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$")) {
-            return new Response(false, "Invalid email format");
+            return Response.failed("Invalid email format");
         }
 
         if (request.getPassword().length() < 4) {
-            return new Response(false, "Password must be at least 4 characters long");
+            return Response.failed("Password must be at least 4 characters long");
         }
         if (!(request.getRole().equals("customer") || request.getRole().equals("admin"))) {
-            return new Response(false, "Role must be customer or admin.");
+            return Response.failed("Role must be customer or admin.");
         }
 
         user = userMapper.toUser(request);
 
         userRepository.addUser(user);
 
-        return new Response(true, "User added successfully.");
+        return Response.ok("User added successfully.");
     }
 }
