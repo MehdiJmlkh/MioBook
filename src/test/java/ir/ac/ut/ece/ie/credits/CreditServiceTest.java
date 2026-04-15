2d4a27c3a3637ce6aa6e49e2450431c66ee79612
Mehdi <mehdijmlkh1382@gmail.com>
2026-04-15 23:35:54 +0330

    test: addCredit

---
+++ src/test/java/ir/ac/ut/ece/ie/credits/CreditServiceTest.java
@@ -0,0 +1,60 @@
+package ir.ac.ut.ece.ie.credits;
+
+import ir.ac.ut.ece.ie.common.NotCustomerException;
+import ir.ac.ut.ece.ie.common.UserNotFoundException;
+import ir.ac.ut.ece.ie.users.Role;
+import ir.ac.ut.ece.ie.users.User;
+import ir.ac.ut.ece.ie.users.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class CreditServiceTest {
+    @MockitoBean
+    private UserRepository userRepository;
+
+    @Autowired
+    private CreditService creditService;
+
+    @Test
+    void addCredit_userNotFound_throwsException() {
+        var request = new AddCreditRequest();
+        assertThrows(UserNotFoundException.class, () -> creditService.addCredit(request));
+    }
+
+    @Test
+    void addCredit_notCustomerUser_throwsException() {
+        var request = new AddCreditRequest();
+        var user = new User();
+        user.setRole(Role.ADMIN);
+
+        when(userRepository.findByUsername(any())).thenReturn(Optional.of(user));
+        assertThrows(NotCustomerException.class, () -> creditService.addCredit(request));
+    }
+
+    @Test
+    void addCredit_validInput_addsCredit() {
+        var request = new AddCreditRequest();
+        request.setUsername("username");
+        request.setCredit(100);
+
+        var user = new User();
+        user.setRole(Role.CUSTOMER);
+        user.setBalance(10);
+
+        when(userRepository.findByUsername(request.getUsername())).thenReturn(Optional.of(user));
+
+        creditService.addCredit(request);
+
+        assertEquals(110, user.getBalance());
+    }
+}
