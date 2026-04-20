adf755953bfdbef5387d3eea9c59ec79320200ce
Mehdi <mehdijmlkh1382@gmail.com>
2026-04-20 15:20:22 +0330

    test: getAllReviews with pagination

--- src/test/java/ir/ac/ut/ece/ie/reviews/ReviewServiceTest.java
+++ src/test/java/ir/ac/ut/ece/ie/reviews/ReviewServiceTest.java
@@ -204,4 +204,45 @@ public class ReviewServiceTest {
         assertEquals(review2.getRate(), reviewDto2.getRate());
         assertEquals(user2.getUsername(), reviewDto2.getUsername());
     }
+
+    @Test
+    void getAllReviews_withPagination_returnsReviewListDto() {
+        var book = new Book();
+
+        var user1 = User.builder().username("user1").build();
+        var review1 = Review.builder()
+                .rate(3)
+                .comment("comment1")
+                .date(LocalDate.of(2001, 1,1))
+                .book(book)
+                .user(user1)
+                .build();
+
+        var user2 = User.builder().username("user2").build();
+        var review2 = Review.builder()
+                .rate(4)
+                .comment("comment2")
+                .date(LocalDate.of(2002, 2,2))
+                .book(book)
+                .user(user2)
+                .build();
+
+        book.getReviews().add(review1);
+        book.getReviews().add(review2);
+
+        when(bookRepository.findByTitle(book.getTitle())).thenReturn(Optional.of(book));
+        when(reviewRepository.findByBook(book, 1, 1)).thenReturn(List.of(review2));
+
+
+        var reviewListDto = reviewService.getAllReviews(book.getTitle(), 1, 1);
+
+        assertEquals(book.getTitle(), reviewListDto.getTitle());
+        assertEquals(3.5, reviewListDto.getAverageRating());
+
+        assertEquals(1, reviewListDto.getReviews().size());
+        var reviewDto1 = reviewListDto.getReviews().get(0);
+        assertEquals(review2.getComment(), reviewDto1.getComment());
+        assertEquals(review2.getRate(), reviewDto1.getRate());
+        assertEquals(user2.getUsername(), reviewDto1.getUsername());
+    }
 }
