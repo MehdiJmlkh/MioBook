e4b907bdbbfc3c9a0a39c696559d43e6ef2cbc44
Mehdi <mehdijmlkh1382@gmail.com>
2026-04-26 12:00:03 +0330

    feat(ui): add onSubmit field to the form component

--- frontend/miobook/src/components/Form/Form.tsx
+++ frontend/miobook/src/components/Form/Form.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { FormEvent, ReactNode } from "react";
 import Button from "../Button";
 import Link from "../Link";
 import "./Form.css";
@@ -11,16 +11,17 @@ export enum FormType {
 interface Props {
   type: FormType;
   children?: ReactNode;
+  onSubmit?: (event: FormEvent) => void;
 }
 
-const Form = ({ type, children }: Props) => {
+const Form = ({ type, children, onSubmit }: Props) => {
   const title = type === FormType.SingIn ? "Sign in" : "Sign Up";
   return (
     <div className="form">
       <h1 className="form__heading">{title}</h1>
       <h2 className="form__sub-heading">MioBook</h2>
 
-      <form action="" className="form__body">
+      <form onSubmit={onSubmit} className="form__body">
         {children}
         <Button className="btn-primary form__btn">{title}</Button>
       </form>
