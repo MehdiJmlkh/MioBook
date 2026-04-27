7fdf5182cbf78cdcdd2a3c3eb9e82604cf5642cc
Mehdi <mehdijmlkh1382@gmail.com>
2026-04-27 22:51:07 +0330

    feat(ui): add className prop to the search box

--- frontend/miobook/src/components/SearchBox/SearchBox.tsx
+++ frontend/miobook/src/components/SearchBox/SearchBox.tsx
@@ -4,25 +4,27 @@ import "./SearchBox.css";
 import FilterSelector from "../FilterSelector";
 import { useState } from "react";
 
-const SearchBox = () => {
+interface Props {
+  className?: string;
+}
+
+const SearchBox = ({ className }: Props) => {
   const filters = ["Author", "Name", "Genre"];
 
   const [selectedFilter, setSelectedFilter] = useState(filters[0]);
   return (
-    <>
-      <div className="search-box">
-        <FilterSelector
-          filters={filters}
-          selectedFilter={selectedFilter}
-          onClickFilter={(filter) => setSelectedFilter(filter)}
-        />
-        <input
-          type="text"
-          className="search-box__search-text"
-          placeholder="Search"
-        />
-      </div>
-    </>
+    <div className={`search-box ${className}`}>
+      <FilterSelector
+        filters={filters}
+        selectedFilter={selectedFilter}
+        onClickFilter={(filter) => setSelectedFilter(filter)}
+      />
+      <input
+        type="text"
+        className="search-box__search-text"
+        placeholder="Search"
+      />
+    </div>
   );
 };
 
