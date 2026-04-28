8256bce92743675c03473e0b3b75ab08a2eac9e2
Mehdi <mehdijmlkh1382@gmail.com>
2026-04-28 22:51:17 +0330

    feat(ui): add onClick prop to button

--- frontend/miobook/src/components/Button/Button.tsx
+++ frontend/miobook/src/components/Button/Button.tsx
@@ -5,11 +5,17 @@ interface Props {
   className?: string;
   children: ReactNode;
   disabled?: boolean;
+  onClick?: () => void;
 }
 
-const Button = ({ className, children, disabled }: Props) => {
+const Button = ({ className, children, disabled, onClick }: Props) => {
   return (
-    <button className={`btn ${className}`} disabled={disabled} type="submit">
+    <button
+      className={`btn ${className}`}
+      disabled={disabled}
+      onClick={onClick}
+      type="submit"
+    >
       {children}
     </button>
   );
