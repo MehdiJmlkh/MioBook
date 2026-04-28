1a701aa54ab1e4c8542f63c36e4d9e588431fa44
Mehdi <mehdijmlkh1382@gmail.com>
2026-04-28 21:54:42 +0330

    feat(ui): add select input component

---
+++ frontend/miobook/src/components/SelectInput/SelectInput.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import Input from "../Input";
+import "./SelectInput.css";
+import { FaCaretDown } from "react-icons/fa";
+
+interface Props {
+  options: string[];
+}
+
+const SelectInput = ({ options }: Props) => {
+  const [isOpen, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+
+  return (
+    <div className="select-input" onClick={() => setOpen(!isOpen)}>
+      <Input readOnly={true} value={value} className="select-input__input" />
+      {isOpen && (
+        <ul className="select-input__dropdown">
+          {options.map((option) => (
+            <li
+              key={option}
+              className="dropdown__item"
+              onClick={() => setValue(option)}
+            >
+              {option}
+            </li>
+          ))}
+        </ul>
+      )}
+      <FaCaretDown className="select-input__icon" />
+    </div>
+  );
+};
+
+export default SelectInput;
