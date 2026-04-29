e29c48cf735bc256bb4046ae4e61dc85a14f9f20
Mehdi <mehdijmlkh1382@gmail.com>
2026-04-29 22:11:39 +0330

    feat(ui): add AddReviewCard to book page

--- frontend/miobook/src/pages/BookPage/BookPage.tsx
+++ frontend/miobook/src/pages/BookPage/BookPage.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import AddReviewCard from "../../components/AddReviewCard";
+import Backdrop from "../../components/Backdrop";
 import BookDetailCard from "../../components/BookDetailCard";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
@@ -5,14 +8,35 @@ import ReviewsBlock from "../../components/ReviewsBlock";
 import "./BookPage.css";
 
 const BookPage = () => {
-  return <body>
-    <Header className="header" />
-    <main>
+  const [openAddReview, setOpenAddReview] = useState(true);
+
+  useEffect(() => {
+    if (openAddReview) {
+      document.body.classList.add("no-scroll");
+    } else {
+      document.body.classList.remove("no-scroll");
+    }
+
+    return () => {
+      document.body.classList.remove("no-scroll");
+    };
+  }, [openAddReview]);
+
+  return (
+    <body>
+      <Header className="header" />
+      <main>
+        <Backdrop enabled={openAddReview} />
+        <AddReviewCard
+          className={openAddReview ? "open" : ""}
+          onClose={() => setOpenAddReview(false)}
+        />
         <BookDetailCard />
-        <ReviewsBlock />
-    </main>
-    <Footer />
-  </body>;
+        <ReviewsBlock onClickAddReview={() => setOpenAddReview(true)} />
+      </main>
+      <Footer />
+    </body>
+  );
 };
 
 export default BookPage;
