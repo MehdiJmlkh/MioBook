9829e4d4d9abed5458cfb6167b51bb4da4691d36
Mehdi <mehdijmlkh1382@gmail.com>
2026-04-29 22:28:09 +0330

    feat(ui): add AddReviewCard responsive to mobile

--- frontend/miobook/src/components/AddReviewCard/AddReviewCard.tsx
+++ frontend/miobook/src/components/AddReviewCard/AddReviewCard.tsx
@@ -13,23 +13,26 @@ interface Props {
 const AddReviewCard = ({ className, onClose }: Props) => {
   return (
     <div className={`add-review ${className}`}>
-      <CloseIcon onClose={onClose} />
-      <h1 className="add-review__heading">Add Review</h1>
-      <img src={NoImage} alt="" className="add-review__img" />
-      <h2 className="add-review__book-title">Book Title</h2>
-      <div className="add-review__rating">
-        <h3 className="add-review__rating__heading">Rating</h3>
-        <RatingStars />
-        <p className="add-review__rating__description">Tap to Rate</p>
+      <div>
+        <CloseIcon onClose={onClose} />
+        <h1 className="add-review__heading">Add Review</h1>
+        <img src={NoImage} alt="" className="add-review__img" />
+        <h2 className="add-review__book-title">Book Title</h2>
+        <div className="add-review__rating">
+          <h3 className="add-review__rating__heading">Rating</h3>
+          <RatingStars />
+          <p className="add-review__rating__description">Tap to Rate</p>
+        </div>
+
+        <TextArea className="add-review__review" rows={7} />
       </div>
+      <div>
+        <Button className="btn-primary" disabled={false}>
+          Submit Reviews
+        </Button>
 
-      <TextArea className="add-review__review" rows={7} />
-
-      <Button className="btn-primary" disabled={false}>
-        Submit Reviews
-      </Button>
-
-      <Button className="btn-secondary add-review__cancel">Cancel</Button>
+        <Button className="btn-secondary add-review__cancel">Cancel</Button>
+      </div>
     </div>
   );
 };
