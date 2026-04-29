8f9435b7c5123fa2d05f412856f90906b0b853f9
Mehdi <mehdijmlkh1382@gmail.com>
2026-04-29 13:57:32 +0330

    feat(ui): add review card component

---
+++ frontend/miobook/src/components/ReviewCard/ReviewCard.tsx
@@ -0,0 +1,19 @@
+import FiveStars from "../FiveStars";
+import "./ReviewCard.css";
+
+const ReviewsCard = () => {
+  return (
+    <div className="review">
+      <div className="review__avatar"> TW </div>
+      <h3 className="review__heading">The Person</h3>
+      <p className="review__content">
+        I bought it 3 weeks ago and now come back just to say "Awesome". I realy
+        enjoy it.
+      </p>
+      <FiveStars className="review__rating" rate={4} />
+      <span className="review__date"> February 20, 2025 </span>
+    </div>
+  );
+};
+
+export default ReviewsCard;
