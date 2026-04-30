dc7bb5b7208c1c20ba13d440e0be58bd9bba42f2
Mehdi <mehdijmlkh1382@gmail.com>
2026-04-30 20:52:51 +0330

    feat(ui): add user account component

---
+++ frontend/miobook/src/components/UserAccount/UserAccount.tsx
@@ -0,0 +1,26 @@
+import { HiOutlineUserCircle } from "react-icons/hi2";
+import "./UserAccount.css";
+import { LuMail } from "react-icons/lu";
+import Button from "../Button";
+
+interface Props {
+  className?: string;
+}
+
+const UserAccount = ({ className }: Props) => {
+  return (
+    <div className={`user-account ${className}`}>
+      <div className="user-account__username">
+        <HiOutlineUserCircle className="user-account__icon" />{" "}
+        <span>SanaNavaei </span>
+      </div>
+      <div className="user-account__email">
+        <LuMail className="user-account__icon" />{" "}
+        <span>sana.sarinavaei@gmail.com</span>
+      </div>
+      <Button className="btn-secondary user-account__btn">Logout</Button>
+    </div>
+  );
+};
+
+export default UserAccount;
