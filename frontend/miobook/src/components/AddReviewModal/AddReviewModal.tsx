2dbf99bdb07eb7bc44cf93d19657925e7eb326a1
Mehdi <mehdijmlkh1382@gmail.com>
2026-04-30 11:56:42 +0330

    refactor(ui): extract modal-slide-over class

--- frontend/miobook/src/components/AddReviewModal/AddReviewModal.tsx
+++ frontend/miobook/src/components/AddReviewModal/AddReviewModal.tsx
@@ -12,7 +12,7 @@ interface Props {
 
 const AddReviewModal = ({ className, onClose }: Props) => {
   return (
-    <div className={`add-review ${className}`}>
+    <div className={`add-review modal-slide-over ${className}`}>
       <div>
         <CloseIcon onClose={onClose} />
         <h1 className="add-review__heading">Add Review</h1>
