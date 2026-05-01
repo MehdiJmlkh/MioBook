5ad8b820158b84e69b042f162a2864d4ce64c0a5
Mehdi <mehdijmlkh1382@gmail.com>
2026-05-01 15:34:20 +0330

    feat(ui): add purchase history page

---
+++ frontend/miobook/src/pages/PurchaseHistoryPage/PurchaseHistoryPage.tsx
@@ -0,0 +1,18 @@
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import HistoryCard from "../../components/HIstoryCard";
+import "./PurchaseHistoryPage.css";
+
+const PurchaseHistoryPage = () => {
+  return (
+    <body>
+      <Header />
+      <main>
+        <HistoryCard />
+      </main>
+      <Footer />
+    </body>
+  );
+};
+
+export default PurchaseHistoryPage;
