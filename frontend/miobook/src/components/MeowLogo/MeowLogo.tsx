ca603e624f2d78dd6c356d451e05181aa92b1863
Mehdi <mehdijmlkh1382@gmail.com>
2026-05-12 23:16:28 +0330

    feat(ui): add navigation for site logo

--- frontend/miobook/src/components/MeowLogo/MeowLogo.tsx
+++ frontend/miobook/src/components/MeowLogo/MeowLogo.tsx
@@ -1,9 +1,11 @@
 import "./MeowLogo.css";
 import Logo from "../../assets/logo.png";
+import { useNavigate } from "react-router-dom";
 
 const MeowIcon = () => {
+  const navigate = useNavigate();
   return (
-    <span className="meow-icon">
+    <span className="meow-icon" onClick={() => navigate("/")}>
       <img src={Logo} alt="" />
     </span>
   );
