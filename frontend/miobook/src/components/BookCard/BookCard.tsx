486cc4deb5c26f6690bff31bff32611d333fc50a
Mehdi <mehdijmlkh1382@gmail.com>
2026-05-12 23:28:33 +0330

    feat(ui): add AddToCartModal to BookCard

--- frontend/miobook/src/components/BookCard/BookCard.tsx
+++ frontend/miobook/src/components/BookCard/BookCard.tsx
@@ -1,17 +1,30 @@
-import "./BookCard.css";
+import { useState } from "react";
 import noImage from "../../assets/no-image.svg";
+import { useNoScroll } from "../../hooks/useNoScroll";
+import { Book } from "../../services/bookService";
+import AddToCartModal from "../AddToCartModal";
+import Backdrop from "../Backdrop";
 import Button from "../Button";
 import FiveStars from "../FiveStars";
 import Price from "../Price";
-import { Book } from "../../services/bookService";
+import "./BookCard.css";
 
 interface Props {
   book?: Book;
 }
 
 function BookCard({ book }: Props) {
+  const [showCartModal, setShowCartModal] = useState(false);
+
+  useNoScroll([showCartModal]);
+
   return (
     <div className="book-card">
+      <Backdrop enabled={showCartModal} />
+      <AddToCartModal
+        className={showCartModal ? "show" : ""}
+        onClose={() => setShowCartModal(false)}
+      />
       <a href="#">
         <img className="book-card__img" src={noImage} alt="Image of the book" />
       </a>
@@ -19,10 +32,15 @@ function BookCard({ book }: Props) {
         <h2 className="book-card__title">{book?.title || "Book title"}</h2>
         <h3 className="book-card__author">{book?.author || "Author McName"}</h3>
         <div className="book-card__badges">
-          <FiveStars className="book-card__stars" rate={book?.averageRating || 0} />
+          <FiveStars
+            className="book-card__stars"
+            rate={book?.averageRating || 0}
+          />
           <Price className="book-card__price">{book?.price || 11.11}</Price>
         </div>
-        <Button className="btn-primary"> Add to Cart</Button>
+        <Button className="btn-primary" onClick={() => setShowCartModal(true)}>
+          Add to Cart
+        </Button>
       </div>
     </div>
   );
