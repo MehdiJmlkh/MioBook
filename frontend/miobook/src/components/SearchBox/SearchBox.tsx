1cc2653878bb94e279680c1916b7084cefb08876
Mehdi <mehdijmlkh1382@gmail.com>
2026-05-12 13:58:41 +0330

    feat(ui): navigate to search result page from search box

--- frontend/miobook/src/components/SearchBox/SearchBox.tsx
+++ frontend/miobook/src/components/SearchBox/SearchBox.tsx
@@ -1,6 +1,8 @@
 import "./SearchBox.css";
 import FilterSelector from "../FilterSelector";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 interface Props {
   className?: string;
@@ -9,20 +11,32 @@ interface Props {
 const SearchBox = ({ className }: Props) => {
   const filters = ["Author", "Name", "Genre"];
 
+  const { register, handleSubmit } = useForm();
+  const navigate = useNavigate();
+
   const [selectedFilter, setSelectedFilter] = useState(filters[0]);
   return (
-    <div className={`search-box ${className}`}>
+    <form
+      className={`search-box ${className}`}
+      onSubmit={handleSubmit((data) => {
+        const params = new URLSearchParams({
+          [selectedFilter.toLowerCase()]: data.search,
+        });
+        navigate({ pathname: "/books", search: params.toString() });
+      })}
+    >
       <FilterSelector
         filters={filters}
         selectedFilter={selectedFilter}
         onClickFilter={(filter) => setSelectedFilter(filter)}
       />
       <input
+        {...register("search")}
         type="text"
         className="search-box__search-text"
         placeholder="Search"
       />
-    </div>
+    </form>
   );
 };
 
