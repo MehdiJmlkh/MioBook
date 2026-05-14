ca3729a63c81440a2b8751d861d0ad968e5a5661
Mehdi <mehdijmlkh1382@gmail.com>
2026-05-14 20:46:05 +0330

    feat(ui): show an empty cart icon when cart is empty

--- frontend/miobook/src/components/Cart/Cart.tsx
+++ frontend/miobook/src/components/Cart/Cart.tsx
@@ -7,6 +7,7 @@ import Table from "../Table";
 import "./Cart.css";
 import { useRemoveCartItem } from "../../queries/carts/useRemoveCartItem";
 import { useAuth } from "../../queries/auth/useAuth";
+import NoProduct from "../../assets/no-product.svg";
 
 const Cart = () => {
   const { data: user } = useAuth();
@@ -18,31 +19,40 @@ const Cart = () => {
 
   return (
     <Card title="Cart" className="cart" icon={<LuShoppingCart />}>
-      <Table>
-        <thead>
-          <tr>
-            <th>Image</th>
-            <th>Name</th>
-            <th>Author</th>
-            <th>Price</th>
-            <th>Borrow Days</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {cart?.items.map((item, i) => (
-            <BookItemRow
-              key={i}
-              item={item}
-              addBtn={true}
-              onClick={() => removeCartItem.mutate(item.bookId)}
-            />
-          ))}
-        </tbody>
-      </Table>
-      <div className="cart__btn">
-        <Button className="btn-primary">Purchase</Button>
-      </div>
+      {cart?.items.length === 0 ? (
+        <div className="empty-list__content">
+          <img className="my-book__no-result" src={NoProduct} alt="" />
+          <p>No Product</p>
+        </div>      ) : (
+        <>
+          <Table>
+            <thead>
+              <tr>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Author</th>
+                <th>Price</th>
+                <th>Borrow Days</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {cart?.items.map((item, i) => (
+                <BookItemRow
+                  key={i}
+                  item={item}
+                  addBtn={true}
+                  onClick={() => removeCartItem.mutate(item.bookId)}
+                />
+              ))}
+            </tbody>
+          </Table>
+
+          <div className="cart__btn">
+            <Button className="btn-primary">Purchase</Button>
+          </div>
+        </>
+      )}
     </Card>
   );
 };
