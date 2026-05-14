1fec948714950702d92bc033fd49d31b86ef86ea
Mehdi <mehdijmlkh1382@gmail.com>
2026-05-14 11:50:08 +0330

    feat(query): call logout hook when click on logout button

--- frontend/miobook/src/components/UserAccount/UserAccount.tsx
+++ frontend/miobook/src/components/UserAccount/UserAccount.tsx
@@ -2,12 +2,15 @@ import { HiOutlineUserCircle } from "react-icons/hi2";
 import "./UserAccount.css";
 import { LuMail } from "react-icons/lu";
 import Button from "../Button";
+import { useLogout } from "../../queries/useLogout";
 
 interface Props {
   className?: string;
 }
 
 const UserAccount = ({ className }: Props) => {
+  const logout = useLogout();
+
   return (
     <div className={`user-account ${className}`}>
       <div className="user-account__username">
@@ -18,7 +21,12 @@ const UserAccount = ({ className }: Props) => {
         <LuMail className="user-account__icon" />{" "}
         <span>sana.sarinavaei@gmail.com</span>
       </div>
-      <Button className="btn-secondary user-account__btn">Logout</Button>
+      <Button
+        className="btn-secondary user-account__btn"
+        onClick={() => logout.mutate()}
+      >
+        Logout
+      </Button>
     </div>
   );
 };
--- frontend/miobook/src/queries/useLogout.ts
+++ frontend/miobook/src/queries/useLogout.ts
@@ -1,10 +1,15 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import authService from "../services/authService";
+import { useNavigate } from "react-router-dom";
 
 export const useLogout = () => {
   const queryClient = useQueryClient();
+  const navigate = useNavigate();
   return useMutation({
     mutationFn: authService.logout,
+    onMutate: () => {
+      navigate("/sign-in");
+    },
     onSuccess: () => {
       queryClient.setQueryData(["auth"], null);
     },
