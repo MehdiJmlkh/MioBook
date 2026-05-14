dece12c91bafd89bef59ce0f37f7f83da995249f
Mehdi <mehdijmlkh1382@gmail.com>
2026-05-14 19:12:45 +0330

    feat(ui): show real data in UserAccount

--- frontend/miobook/src/components/UserAccount/UserAccount.tsx
+++ frontend/miobook/src/components/UserAccount/UserAccount.tsx
@@ -3,23 +3,25 @@ import "./UserAccount.css";
 import { LuMail } from "react-icons/lu";
 import Button from "../Button";
 import { useLogout } from "../../queries/auth/useLogout";
+import { useAuth } from "../../queries/auth/useAuth";
 
 interface Props {
   className?: string;
 }
 
 const UserAccount = ({ className }: Props) => {
+  const {data: user} = useAuth();
   const logout = useLogout();
 
   return (
     <div className={`user-account ${className}`}>
       <div className="user-account__username">
         <HiOutlineUserCircle className="user-account__icon" />{" "}
-        <span>SanaNavaei </span>
+        <span>{user?.username}</span>
       </div>
       <div className="user-account__email">
         <LuMail className="user-account__icon" />{" "}
-        <span>sana.sarinavaei@gmail.com</span>
+        <span>{user?.email}</span>
       </div>
       <Button
         className="btn-secondary user-account__btn"
