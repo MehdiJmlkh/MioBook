18d4994a12de7bc7162e16c347e142b5809116b5
Mehdi <mehdijmlkh1382@gmail.com>
2026-05-18 16:12:24 +0330

    feat(api): add entity annotation for Author

--- backend/src/main/java/ir/ac/ut/ece/ie/authors/Author.java
+++ backend/src/main/java/ir/ac/ut/ece/ie/authors/Author.java
@@ -1,5 +1,6 @@
 package ir.ac.ut.ece.ie.authors;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,12 +9,29 @@ import java.time.LocalDate;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "authors")
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "pen_name")
     private String penName;
+
+    @Column(name = "nationality")
     private String nationality;
+
+    @Column(name = "born")
     private LocalDate born;
+
+    @Column(name = "died")
     private LocalDate died;
+
+    @Column(name = "image_link")
     private String imageLink;
 }
