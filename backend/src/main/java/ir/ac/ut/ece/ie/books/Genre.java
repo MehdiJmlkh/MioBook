543214efdda1205f472f3a7e5df3184f9e5d2d38
Mehdi <mehdijmlkh1382@gmail.com>
2026-05-18 16:42:08 +0330

    feat(api): add entity annotation for Genre

--- backend/src/main/java/ir/ac/ut/ece/ie/books/Genre.java
+++ backend/src/main/java/ir/ac/ut/ece/ie/books/Genre.java
@@ -1,5 +1,6 @@
 package ir.ac.ut.ece.ie.books;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,8 +8,15 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@Entity
+@Table(name = "genres")
 public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "name")
     private String name;
 
     public Genre(String name) {
