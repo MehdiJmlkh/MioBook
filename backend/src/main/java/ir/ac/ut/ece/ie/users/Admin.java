1bb4fcd34ed88731feb6e02107b3b655d39e926a
Mehdi <mehdijmlkh1382@gmail.com>
2026-05-18 16:26:28 +0330

    feat(api): add entity annotation for Admin

--- backend/src/main/java/ir/ac/ut/ece/ie/users/Admin.java
+++ backend/src/main/java/ir/ac/ut/ece/ie/users/Admin.java
@@ -1,4 +1,17 @@
 package ir.ac.ut.ece.ie.users;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "admins")
+@PrimaryKeyJoinColumn(name = "id")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Admin extends User {
 }
