601ec8da11b918c61fc0136104ac0e0ff5408249
Mehdi <mehdijmlkh1382@gmail.com>
2026-05-18 16:57:21 +0330

    feat(api): add one to many relationship from Customer to Purchase

--- backend/src/main/java/ir/ac/ut/ece/ie/users/Customer.java
+++ backend/src/main/java/ir/ac/ut/ece/ie/users/Customer.java
@@ -1,11 +1,11 @@
 package ir.ac.ut.ece.ie.users;
 
 import ir.ac.ut.ece.ie.carts.Cart;
-import jakarta.persistence.Entity;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.PrimaryKeyJoinColumn;
-import jakarta.persistence.Table;
+import ir.ac.ut.ece.ie.purchases.Purchase;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.Set;
 
 @Getter
 @Setter
@@ -18,6 +18,9 @@ public class Customer extends User {
 
     @OneToOne(mappedBy = "user")
     private Cart cart;
+
+    @OneToMany(mappedBy = "user")
+    private Set<Purchase> purchases;
 
     public void addCredit(Integer amount) {
         wallet.addCredit(amount);
