633e91fdd6b7cf6abb4ae2629ba0ea1dcf849630
Mehdi <mehdijmlkh1382@gmail.com>
2026-05-18 16:23:09 +0330

    feat(api): add entity annotation for Address

--- backend/src/main/java/ir/ac/ut/ece/ie/users/Address.java
+++ backend/src/main/java/ir/ac/ut/ece/ie/users/Address.java
@@ -1,11 +1,24 @@
 package ir.ac.ut.ece.ie.users;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "addresses")
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "country")
     private String country;
+
+    @Column(name = "city")
     private String city;
 }
