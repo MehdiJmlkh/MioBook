11ddd81cfca5eb181ebe9609af327ca52b08e929
Mehdi <mehdijmlkh1382@gmail.com>
2026-05-18 16:21:54 +0330

    feat(api): add entity annotation for User

--- backend/src/main/java/ir/ac/ut/ece/ie/users/User.java
+++ backend/src/main/java/ir/ac/ut/ece/ie/users/User.java
@@ -1,5 +1,6 @@
 package ir.ac.ut.ece.ie.users;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 @Builder
@@ -7,9 +8,26 @@ import lombok.*;
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table(name = "users")
+@Inheritance(strategy = InheritanceType.JOINED)
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username")
     private String username;
+
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "email")
     private String email;
+
+
+    @ManyToOne
+    @JoinColumn(name = "address_id")
     private Address address;
 }
