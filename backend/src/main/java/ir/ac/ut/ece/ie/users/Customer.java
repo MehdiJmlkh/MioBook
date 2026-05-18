948d54e29fe80b76daa277609c2e144de70f490d
Mehdi <mehdijmlkh1382@gmail.com>
2026-05-18 16:25:41 +0330

    feat(api): add entity annotation for Customer

--- backend/src/main/java/ir/ac/ut/ece/ie/users/Customer.java
+++ backend/src/main/java/ir/ac/ut/ece/ie/users/Customer.java
@@ -1,12 +1,22 @@
 package ir.ac.ut.ece.ie.users;
 
 import ir.ac.ut.ece.ie.carts.Cart;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
 import lombok.*;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "customers")
+@PrimaryKeyJoinColumn(name = "id")
 public class Customer extends User {
+    @OneToOne(mappedBy = "customer")
     private Wallet wallet = new Wallet();
+
+    @OneToOne(mappedBy = "user")
     private Cart cart;
 
     public void addCredit(Integer amount) {
