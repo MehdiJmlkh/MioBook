abc06e6b473f2f617505a16ac0425adbbc3fc309
Mehdi <mehdijmlkh1382@gmail.com>
2026-05-18 16:22:55 +0330

    feat(api): add entity annotation for Review

--- backend/src/main/java/ir/ac/ut/ece/ie/reviews/Review.java
+++ backend/src/main/java/ir/ac/ut/ece/ie/reviews/Review.java
@@ -2,6 +2,7 @@ package ir.ac.ut.ece.ie.reviews;
 
 import ir.ac.ut.ece.ie.books.Book;
 import ir.ac.ut.ece.ie.users.User;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -11,10 +12,28 @@ import java.time.LocalDate;
 @Builder
 @Getter
 @Setter
+@Entity
+@Table(name = "reviews")
 public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "rate")
     private Integer rate;
+
+    @Column(name = "comment")
     private String comment;
+
+    @Column(name = "date")
     private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
     private Book book;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
     private User user;
 }
