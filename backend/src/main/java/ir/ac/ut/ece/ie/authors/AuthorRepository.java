36654f250884b78aae90d4c9be9e0872a206abc3
Mehdi <mehdijmlkh1382@gmail.com>
2026-05-19 13:46:33 +0330

    refactor(api): change AuthorRepository to extend JpaRepository

--- backend/src/main/java/ir/ac/ut/ece/ie/authors/AuthorRepository.java
+++ backend/src/main/java/ir/ac/ut/ece/ie/authors/AuthorRepository.java
@@ -1,35 +1,9 @@
 package ir.ac.ut.ece.ie.authors;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
-@Repository
-public class AuthorRepository {
-    private final Set<Author> authors = new LinkedHashSet<>();
-    private Long lastId = 0L;
-
-    public void addAuthor(Author author) {
-        author.setId(lastId++);
-        authors.add(author);
-    }
-
-    public Optional<Author> findByName(String name) {
-        return authors.stream()
-                .filter(author -> author.getName().equals(name))
-                .findFirst();
-    }
-
-    public Optional<Author> findById(Long id) {
-        return authors.stream()
-                .filter(author -> author.getId().equals(id))
-                .findFirst();
-    }
-
-    public List<Author> getAll() {
-        return authors.stream().toList();
-    }
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+    Optional<Author> findByName(String name);
 }
