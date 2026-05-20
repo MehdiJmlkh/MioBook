e87d127c0a57fe6f4d9733470ab4e322a3ef7a8f
Mehdi <mehdijmlkh1382@gmail.com>
2026-05-20 11:34:44 +0330

    refactor(api): remove unused imports

--- backend/src/main/java/ir/ac/ut/ece/ie/users/CustomerRepository.java
+++ backend/src/main/java/ir/ac/ut/ece/ie/users/CustomerRepository.java
@@ -2,7 +2,6 @@ package ir.ac.ut.ece.ie.users;
 
 import org.springframework.data.repository.CrudRepository;
 
-
 import java.util.Optional;
 
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
--- backend/src/main/java/ir/ac/ut/ece/ie/users/UserRepository.java
+++ backend/src/main/java/ir/ac/ut/ece/ie/users/UserRepository.java
@@ -1,11 +1,8 @@
 package ir.ac.ut.ece.ie.users;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.LinkedHashSet;
 import java.util.Optional;
-import java.util.Set;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findByUsername(String username);
