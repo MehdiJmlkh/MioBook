e31cd36d259947ed92174948aeb60e0c65a263f3
Mehdi <mehdijmlkh1382@gmail.com>
2026-05-22 21:45:56 +0330

    refactor(api): calculate getTopRated and getNewReleases using SQL

--- backend/src/main/java/ir/ac/ut/ece/ie/books/BookRepository.java
+++ backend/src/main/java/ir/ac/ut/ece/ie/books/BookRepository.java
@@ -5,25 +5,18 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.*;
 
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
     Optional<Book> findByTitle(String title);
     Page<Book> findByAuthor(Author author, Pageable pageable);
-    default List<Book> getTopRated(int limit) {
-        var books = findAll();
-        return books.stream()
-                .sorted(Comparator.comparing(Book::getAverageRating).reversed())
-                .limit(limit)
-                .toList();
-    }
 
-    default List<Book> getNewReleases(int limit) {
-        var books = findAll();
-        return books.stream()
-                .sorted(Comparator.comparing(Book::getYear).reversed())
-                .limit(limit)
-                .toList();
-    }
+    @Query(value = "SELECT * FROM books ORDER BY average_rating DESC LIMIT :limit", nativeQuery = true)
+    List<Book> getTopRated(@Param("limit") int limit);
+
+    @Query(value = "SELECT * FROM books ORDER BY year DESC LIMIT :limit", nativeQuery = true)
+    List<Book> getNewReleases(@Param("limit") int limit);
 }
