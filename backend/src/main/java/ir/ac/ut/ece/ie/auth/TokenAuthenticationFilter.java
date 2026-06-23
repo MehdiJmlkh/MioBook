5019742b22602777d1fce7928d20f51b8c535199
Mehdi <mehdijmlkh1382@gmail.com>
2026-06-23 12:39:24 +0330

    feat(api): add TokenAuthenticationFilter

---
+++ backend/src/main/java/ir/ac/ut/ece/ie/auth/TokenAuthenticationFilter.java
@@ -0,0 +1,52 @@
+package ir.ac.ut.ece.ie.auth;
+
+import ir.ac.ut.ece.ie.sessions.SessionService;
+import ir.ac.ut.ece.ie.sessions.UserSession;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Component
+@AllArgsConstructor
+public class TokenAuthenticationFilter extends OncePerRequestFilter {
+    private final SessionService sessionService;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String header = request.getHeader("Authorization");
+
+        if (header != null && header.startsWith("Bearer ")) {
+            String token = header.substring(7);
+
+            UserSession session = sessionService.getSession(token);
+
+            if (session != null) {
+                var authentication =
+                        new UsernamePasswordAuthenticationToken(
+                                session,
+                                null,
+                                Collections.emptyList());
+
+                SecurityContextHolder
+                        .getContext()
+                        .setAuthentication(authentication);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
