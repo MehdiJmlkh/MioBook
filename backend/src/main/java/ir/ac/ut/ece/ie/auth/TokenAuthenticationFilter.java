d9be73d2b8e76cc10d9fed9a5056916e49d87b47
Mehdi <mehdijmlkh1382@gmail.com>
2026-06-24 14:26:43 +0330

    refactor(api): remove unused UserSession

--- backend/src/main/java/ir/ac/ut/ece/ie/auth/TokenAuthenticationFilter.java
+++ backend/src/main/java/ir/ac/ut/ece/ie/auth/TokenAuthenticationFilter.java
@@ -1,7 +1,6 @@
 package ir.ac.ut.ece.ie.auth;
 
 import ir.ac.ut.ece.ie.sessions.SessionService;
-import ir.ac.ut.ece.ie.sessions.UserSession;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
