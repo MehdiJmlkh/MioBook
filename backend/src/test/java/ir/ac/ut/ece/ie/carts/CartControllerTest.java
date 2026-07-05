b56f301a396e3c4144f490be4ff3001ad5ea8b1f
Mehdi <mehdijmlkh1382@gmail.com>
2026-07-05 12:15:50 +0330

    test(api): verify only customers can access carts endpoints using one sample url

--- backend/src/test/java/ir/ac/ut/ece/ie/carts/CartControllerTest.java
+++ backend/src/test/java/ir/ac/ut/ece/ie/carts/CartControllerTest.java
@@ -1,41 +1,39 @@
 package ir.ac.ut.ece.ie.carts;
 
-import ir.ac.ut.ece.ie.utils.EndpointTestCase;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.stream.Stream;
-
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class CartControllerTest {
+    @MockitoBean
+    CartService cartService;
     @Autowired
     MockMvc mockMvc;
 
-    @ParameterizedTest
-    @MethodSource("protectedEndpoints")
-    @WithMockUser(roles = "ADMIN")
-    void admin_cannot_access_protected_endpoint(EndpointTestCase tc) throws Exception {
-        mockMvc.perform(request(tc.method(), tc.url()))
-                .andExpect(status().isForbidden());
+    @Test
+    @WithMockUser(roles = "CUSTOMER")
+    void carts_access_allowed_for_customer() throws Exception {
+        when(cartService.getCart(any())).thenReturn(new CartDto());
+
+        mockMvc.perform(get("/carts/username"))
+                .andExpect(status().isOk());
     }
 
-    static Stream<EndpointTestCase> protectedEndpoints() {
-        return Stream.of(
-                new EndpointTestCase("/carts/username", HttpMethod.GET),
-                new EndpointTestCase("/carts/items", HttpMethod.POST),
-                new EndpointTestCase("/carts/borrowed-items", HttpMethod.POST),
-                new EndpointTestCase("/carts/items/1", HttpMethod.DELETE),
-                new EndpointTestCase("/carts/purchase", HttpMethod.POST)
-        );
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    void carts_access_denied_for_admin() throws Exception {
+        mockMvc.perform(get("/carts/username"))
+                .andExpect(status().isForbidden());
     }
 }
--- backend/src/test/java/ir/ac/ut/ece/ie/utils/EndpointTestCase.java
+++
@@ -1,6 +0,0 @@
-package ir.ac.ut.ece.ie.utils;
-
-import org.springframework.http.HttpMethod;
-
-public record EndpointTestCase(String url, HttpMethod method) {
-}
