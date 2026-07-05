4dac744586df825da5a254d30a89922a9a763e84
Mehdi <mehdijmlkh1382@gmail.com>
2026-07-05 12:27:35 +0330

    test(api): verify only customers can access purchases endpoints

--- backend/src/main/java/ir/ac/ut/ece/ie/purchases/PurchaseHistoryDto.java
+++ backend/src/main/java/ir/ac/ut/ece/ie/purchases/PurchaseHistoryDto.java
@@ -1,12 +1,16 @@
 package ir.ac.ut.ece.ie.purchases;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PurchaseHistoryDto {
     private String username;
     private List<PurchaseDto> purchaseHistory;
--- backend/src/test/java/ir/ac/ut/ece/ie/purchases/PurchaseControllerTest.java
+++ backend/src/test/java/ir/ac/ut/ece/ie/purchases/PurchaseControllerTest.java
@@ -1,11 +1,9 @@
-package ir.ac.ut.ece.ie.credits;
+package ir.ac.ut.ece.ie.purchases;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -13,35 +11,29 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class CreditControllerTest {
+public class PurchaseControllerTest {
     @MockitoBean
-    CreditService creditService;
+    PurchaseService purchaseService;
     @Autowired
     MockMvc mockMvc;
-
-    @Autowired
-    ObjectMapper objectMapper;
 
     @Test
     @WithMockUser(roles = "CUSTOMER")
     void credits_access_allowed_for_customer() throws Exception {
-        when(creditService.addCredit(any())).thenReturn(0);
+        when(purchaseService.getAllPurchases(any())).thenReturn(new PurchaseHistoryDto());
 
-        mockMvc.perform(post("/credits")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(new AddCreditRequest())))
+        mockMvc.perform(get("/purchases/username"))
                 .andExpect(status().isOk());
     }
 
     @Test
     @WithMockUser(roles = "ADMIN")
     void credits_access_denied_for_admin() throws Exception {
-        mockMvc.perform(post("/credits"))
+        mockMvc.perform(get("/purchases/username"))
                 .andExpect(status().isForbidden());
     }
 }
