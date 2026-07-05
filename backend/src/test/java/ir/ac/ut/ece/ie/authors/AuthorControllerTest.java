c576676f01aad2ffb4e4d7b00cf104094b5f8688
Mehdi <mehdijmlkh1382@gmail.com>
2026-07-05 11:35:05 +0330

    test(api): verify admin access control

---
+++ backend/src/test/java/ir/ac/ut/ece/ie/authors/AuthorControllerTest.java
@@ -0,0 +1,45 @@
+package ir.ac.ut.ece.ie.authors;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AuthorControllerTest {
+    @MockitoBean
+    AuthorService authorService;
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    void addAuthor_adminUser_okStatus() throws Exception {
+        when(authorService.addAuthor(any())).thenReturn(new AuthorDto());
+
+        mockMvc.perform(post("/authors")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(new AddAuthorRequest())))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(roles = "CUSTOMER")
+    void addAuthor_customerUser_forbiddenStatus() throws Exception {
+        mockMvc.perform(post("/authors"))
+                .andExpect(status().isForbidden());
+    }
+}
