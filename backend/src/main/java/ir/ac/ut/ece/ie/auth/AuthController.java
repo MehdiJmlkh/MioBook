be6c325cdbbf4890eb689f658017516925663bf6
Mehdi <mehdijmlkh1382@gmail.com>
2026-08-04 13:54:56 +0330

    refactor(api): extract createRefreshTokenCookie

--- backend/src/main/java/ir/ac/ut/ece/ie/auth/AuthController.java
+++ backend/src/main/java/ir/ac/ut/ece/ie/auth/AuthController.java
@@ -2,12 +2,12 @@ package ir.ac.ut.ece.ie.auth;
 
 import ir.ac.ut.ece.ie.common.ErrorDto;
 import ir.ac.ut.ece.ie.config.JwtConfig;
-import ir.ac.ut.ece.ie.users.User;
 import ir.ac.ut.ece.ie.users.UserRepository;
 import ir.ac.ut.ece.ie.users.UserService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.checkerframework.checker.nullness.qual.NonNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -42,11 +42,7 @@ public class AuthController {
         var accessToken = jwtService.generateAccessToken(user);
         var refreshToken = jwtService.generateRefreshToken(user);
 
-        var cookie = new Cookie("refreshToken", refreshToken.toString());
-        cookie.setHttpOnly(true);
-        cookie.setPath("/auth/refresh");
-        cookie.setMaxAge(jwtConfig.getRefreshTokenExpiration());
-        cookie.setSecure(true);
+        var cookie = createRefreshTokenCookie(refreshToken);
         response.addCookie(cookie);
 
         return ResponseEntity.ok(new JwtResponse(accessToken.toString()));
@@ -97,14 +93,19 @@ public class AuthController {
         var accessToken = jwtService.generateAccessToken(user);
         var refreshToken = jwtService.generateRefreshToken(user);
 
+        var cookie = createRefreshTokenCookie(refreshToken);
+        response.addCookie(cookie);
+
+        return ResponseEntity.ok(new JwtResponse(accessToken.toString()));
+    }
+
+    private @NonNull Cookie createRefreshTokenCookie(Jwt refreshToken) {
         var cookie = new Cookie("refreshToken", refreshToken.toString());
         cookie.setHttpOnly(true);
         cookie.setPath("/auth/refresh");
         cookie.setMaxAge(jwtConfig.getRefreshTokenExpiration());
         cookie.setSecure(true);
-        response.addCookie(cookie);
-
-        return ResponseEntity.ok(new JwtResponse(accessToken.toString()));
+        return cookie;
     }
 
     @ExceptionHandler(BadCredentialsException.class)
