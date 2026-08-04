079e49e35dfc1811fe5af5e70697a733da1f886e
Mehdi <mehdijmlkh1382@gmail.com>
2026-08-04 12:04:52 +0330

    refactor(api): use FrontendConfig for setting cors config

--- backend/src/main/java/ir/ac/ut/ece/ie/config/CorsConfig.java
+++ backend/src/main/java/ir/ac/ut/ece/ie/config/CorsConfig.java
@@ -1,20 +1,24 @@
 package ir.ac.ut.ece.ie.config;
 
+import lombok.AllArgsConstructor;
+import org.checkerframework.checker.nullness.qual.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@AllArgsConstructor
 @Configuration
 public class CorsConfig {
+    private final FrontendConfig frontendConfig;
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(frontendConfig.getUrl())
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
