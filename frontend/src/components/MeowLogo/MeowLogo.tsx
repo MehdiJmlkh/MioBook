ea642d77f9a84f2a89770a0b0fcc826c89c2096f
Mehdi <mehdijmlkh1382@gmail.com>
2026-08-28 21:52:29 +0330

    fix(api): optimize svg images

--- frontend/src/components/MeowLogo/MeowLogo.tsx
+++ frontend/src/components/MeowLogo/MeowLogo.tsx
@@ -1,5 +1,5 @@
 import "./MeowLogo.css";
-import Logo from "../../assets/logo.png";
+import Logo from "../../assets/logo.svg";
 import { useNavigate } from "react-router-dom";
 
 const MeowIcon = () => {
