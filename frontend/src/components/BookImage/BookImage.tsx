b743f3f898fdede94a37a3b2f52adc640a14c8b8
Mehdi <mehdijmlkh1382@gmail.com>
2026-08-28 09:39:22 +0330

    feat(ui): show noImage if showing the image cause an error

--- frontend/src/components/BookImage/BookImage.tsx
+++ frontend/src/components/BookImage/BookImage.tsx
@@ -1,18 +1,30 @@
 import "./BookImage.css";
+import defaultNoImage from "../../assets/no-image.svg";
 
 interface Props {
   imageLink?: string;
   className?: string;
+  noImage?: string;
 }
 
-const BookImage = ({ imageLink, className }: Props) => {
+const BookImage = ({
+  imageLink,
+  className,
+  noImage = defaultNoImage,
+}: Props) => {
   const baseUrl = window.location.origin;
+
+  const imageSrc = imageLink ? imageLink : noImage;
 
   return (
     <img
       className={className}
-      src={`${baseUrl}/public/${imageLink || "no-image.svg"}`}
+      src={imageSrc}
       alt="Image of the book"
+      onError={(error) => {
+        error.currentTarget.onerror = null;
+        error.currentTarget.src = noImage;
+      }}
     />
   );
 };
