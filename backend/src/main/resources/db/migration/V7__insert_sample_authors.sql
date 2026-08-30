288489bef01bf94d854da57c0cbd8ca52e72e25c
Mehdi <mehdijmlkh1382@gmail.com>
2026-08-30 22:46:51 +0330

    feat(api): update sample data

--- backend/src/main/resources/db/migration/V7__insert_sample_authors.sql
+++ backend/src/main/resources/db/migration/V7__insert_sample_authors.sql
@@ -1,9 +1,9 @@
 START TRANSACTION;
 
-INSERT INTO authors (id, name, pen_name, nationality, born, died, image_link, admin_id) VALUES (1, 'Elara Vance', 'E. V. Nocturne', 'Canadian', '1982-04-12', NULL, '/src/assets/author.svg', 1);
-INSERT INTO authors (id, name, pen_name, nationality, born, died, image_link, admin_id) VALUES (2, 'Julian Thorne', 'J. T. Sterling', 'British', '1945-11-20', '2018-09-05', '/src/assets/author.svg', 1);
-INSERT INTO authors (id, name, pen_name, nationality, born, died, image_link, admin_id) VALUES (3, 'Hiroshi Tanaka', 'H. T. Sora', 'Japanese', '1990-02-28', NULL, '/src/assets/author.svg', 1);
-INSERT INTO authors (id, name, pen_name, nationality, born, died, image_link, admin_id) VALUES (4, 'Beatrix O''Malley', 'Bea O''Malley', 'Irish', '1930-07-14', '2021-03-30', '/src/assets/author.svg', 1);
-INSERT INTO authors (id, name, pen_name, nationality, born, died, image_link, admin_id) VALUES (5, 'Marcus Valerius', 'M. V. Aurelius', 'Italian', '1975-08-09', NULL, '/src/assets/author.svg', 1);
+INSERT INTO authors (id, name, pen_name, nationality, born, died, image_link, admin_id) VALUES (1, 'Elara Vance', 'E. V. Nocturne', 'Canadian', '1982-04-12', NULL, '', 1);
+INSERT INTO authors (id, name, pen_name, nationality, born, died, image_link, admin_id) VALUES (2, 'Julian Thorne', 'J. T. Sterling', 'British', '1945-11-20', '2018-09-05', '', 1);
+INSERT INTO authors (id, name, pen_name, nationality, born, died, image_link, admin_id) VALUES (3, 'Hiroshi Tanaka', 'H. T. Sora', 'Japanese', '1990-02-28', NULL, '', 1);
+INSERT INTO authors (id, name, pen_name, nationality, born, died, image_link, admin_id) VALUES (4, 'Beatrix O''Malley', 'Bea O''Malley', 'Irish', '1930-07-14', '2021-03-30', '', 1);
+INSERT INTO authors (id, name, pen_name, nationality, born, died, image_link, admin_id) VALUES (5, 'Marcus Valerius', 'M. V. Aurelius', 'Italian', '1975-08-09', NULL, '', 1);
 
 COMMIT;
